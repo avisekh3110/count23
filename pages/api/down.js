2532a2cd7138212a59ce6c478d9d63ef96dbfa87
mind0bender <mind0bender@gmail.com>
2022-07-16 14:26:09 +0530

    fix db connection

--- pages/api/down.js
+++ pages/api/down.js
@@ -1,5 +1,6 @@
 import { isToday } from "date-fns";
 import User from "../../models/user";
+import connectDB from "../../utils/connectMongoose";
 import logger from "../../utils/logger";
 
 const downHandler = (req, res) => {
@@ -8,103 +9,117 @@ const downHandler = (req, res) => {
     method: req.method,
   });
   downLogger.info({ data: { path: "/api/down", method: req.method } });
-
-  if (req.method === "POST") {
-    downLogger.info({ data: { path: "/api/down", value: 1 } });
-    User.exists({})
-      .then((exists) => {
-        downLogger.info({ data: { path: "/api/down", value: 2 } });
-        if (exists) {
-          User.findOne({})
-            .then((user) => {
-              downLogger.info({ data: { path: "/api/down", value: 3 } });
-              downLogger.info({
-                data: {
-                  isToday: isToday(new Date(user.lastUpdated)),
-                  lastUpdated: user.lastUpdated,
-                },
-              });
-              if (isToday(new Date(user.lastUpdated))) {
-                downLogger.info({ data: { path: "/api/down", value: 4 } });
-
-                downLogger.info({
-                  data: { msg: "User already updated today" },
-                });
-                res.status(200).json({
-                  result: false,
-                  errs: ["User already updated today"],
-                  message: "User already updated today",
-                });
-              } else {
-                downLogger.info({ data: { path: "/api/down", value: 5 } });
-                User.findOneAndUpdate(
-                  {},
-                  {
-                    lastUpdated: new Date().toUTCString(),
-                    $inc: { wastedDays: 1 },
-                  }
-                )
-                  .then((updatedUser) => {
-                    downLogger.info({ data: { path: "/api/down", value: 6 } });
+  connectDB()
+    .then(() => {
+      if (req.method === "POST") {
+        downLogger.info({ data: { path: "/api/down", value: 1 } });
+        User.exists({})
+          .then((exists) => {
+            downLogger.info({ data: { path: "/api/down", value: 2 } });
+            if (exists) {
+              User.findOne({})
+                .then((user) => {
+                  downLogger.info({ data: { path: "/api/down", value: 3 } });
+                  downLogger.info({
+                    data: {
+                      isToday: isToday(new Date(user.lastUpdated)),
+                      lastUpdated: user.lastUpdated,
+                    },
+                  });
+                  if (isToday(new Date(user.lastUpdated))) {
+                    downLogger.info({ data: { path: "/api/down", value: 4 } });
 
                     downLogger.info({
-                      data: {
-                        user: updatedUser,
-                        message: "Wasted successfully",
-                      },
+                      data: { msg: "User already updated today" },
                     });
-                    res.send({
-                      user: updatedUser,
-                      message: "Wasted successfully",
-                      result: true,
-                    });
-                  })
-                  .catch((errFOU) => {
-                    downLogger.info({ data: { path: "/api/down", value: 7 } });
-
-                    downLogger.error({ data: { err: errFOU } });
-                    res.send({
-                      message: "Error updating user",
-                      errs: ["Error updating user"],
+                    res.status(200).json({
                       result: false,
+                      errs: ["User already updated today"],
+                      message: "User already updated today",
                     });
+                  } else {
+                    downLogger.info({ data: { path: "/api/down", value: 5 } });
+                    User.findOneAndUpdate(
+                      {},
+                      {
+                        lastUpdated: new Date().toUTCString(),
+                        $inc: { wastedDays: 1 },
+                      }
+                    )
+                      .then((updatedUser) => {
+                        downLogger.info({
+                          data: { path: "/api/down", value: 6 },
+                        });
+
+                        downLogger.info({
+                          data: {
+                            user: updatedUser,
+                            message: "Wasted successfully",
+                          },
+                        });
+                        res.send({
+                          user: updatedUser,
+                          message: "Wasted successfully",
+                          result: true,
+                        });
+                      })
+                      .catch((errFOU) => {
+                        downLogger.info({
+                          data: { path: "/api/down", value: 7 },
+                        });
+
+                        downLogger.error({ data: { err: errFOU } });
+                        res.send({
+                          message: "Error updating user",
+                          errs: ["Error updating user"],
+                          result: false,
+                        });
+                      });
+                  }
+                })
+                .catch((errFOU) => {
+                  downLogger.info({ data: { path: "/api/down", value: 8 } });
+
+                  downLogger.error({ data: { err: errFOU } });
+                  res.send({
+                    message: "Error finding user",
+                    errs: ["Error finding user"],
+                    result: false,
                   });
-              }
-            })
-            .catch((errFOU) => {
-              downLogger.info({ data: { path: "/api/down", value: 8 } });
+                });
+            } else {
+              downLogger.info({ data: { path: "/api/down", value: 9 } });
 
-              downLogger.error({ data: { err: errFOU } });
-              res.send({
-                message: "Error finding user",
-                errs: ["Error finding user"],
-                result: false,
+              downLogger.error({
+                data: {
+                  message: "User does not exist",
+                  result: false,
+                },
               });
-            });
-        } else {
-          downLogger.info({ data: { path: "/api/down", value: 9 } });
-
-          downLogger.error({
-            data: {
-              message: "User does not exist",
+              res.send({
+                message: "User does not exist",
+                result: false,
+                errs: ["User does not exist"],
+              });
+            }
+          })
+          .catch((errFOU) => {
+            downLogger.info({ data: { path: "/api/down", value: 10 } });
+            res.send({
+              message: "Error finding user",
+              errs: ["Error finding user"],
               result: false,
-            },
+            });
           });
-          res.send({
-            message: "User does not exist",
-            result: false,
-            errs: ["User does not exist"],
-          });
-        }
-      })
-      .catch((errFOU) => {
-        downLogger.info({ data: { path: "/api/down", value: 10 } });
-        res.send({
-          message: "Error finding user",
-          errs: ["Error finding user"],
-          result: false,
-        });
+      }
+    })
+    .catch((err) => {
+      downLogger.error({ data: { err: err } });
+      res.send({
+        message: "Error connecting to database",
+        errs: ["Error connecting to database"],
+        result: false,
       });
-  }
+    });
 };
 export default downHandler;
--- pages/api/up.js
+++ pages/api/up.js
@@ -1,96 +1,107 @@
 import { isToday } from "date-fns";
 import User from "../../models/user";
+import connectDB from "../../utils/connectMongoose";
 import logger from "../../utils/logger";
 import { getMarks } from "../../utils/marks";
-import { log } from "next-axiom";
 
 const upHandler = (req, res) => {
   const upLogger = logger.child({
     name: "/api/up",
     method: req.method,
   });
-  upLogger.info({ data: { path: "/api/up", method: req.method } });
-  if (req.method === "POST") {
-    User.exists({})
-      .then((exists) => {
-        if (exists) {
-          User.findOne({})
-            .then((user) => {
-              upLogger.info({
+  connectDB()
+    .then(() => {
+      upLogger.info({ data: { path: "/api/up", method: req.method } });
+      if (req.method === "POST") {
+        User.exists({})
+          .then((exists) => {
+            if (exists) {
+              User.findOne({})
+                .then((user) => {
+                  upLogger.info({
+                    data: {
+                      isToday: isToday(new Date(user.lastUpdated)),
+                      lastUpdated: user.lastUpdated,
+                    },
+                  });
+                  if (isToday(new Date(user.lastUpdated))) {
+                    upLogger.info("User already updated today");
+                    res.status(200).json({
+                      result: false,
+                      errs: ["User already updated today"],
+                      message: "User already updated today",
+                    });
+                  } else {
+                    User.findOneAndUpdate(
+                      {},
+                      { lastUpdated: new Date().toUTCString() }
+                    )
+                      .then((updatedUser) => {
+                        upLogger.info({
+                          data: {
+                            user: {
+                              lastUpdated: updatedUser.lastUpdated,
+                              marks: getMarks(updatedUser.wastedDays),
+                              wastedDays: updatedUser.wastedDays,
+                            },
+                            message: "Utilised successfully",
+                          },
+                        });
+                        res.send({
+                          user: updatedUser,
+                          message: "Utilised successfully",
+                          result: true,
+                        });
+                      })
+                      .catch((errFOU) => {
+                        upLogger.error({ data: { err: errFOU } });
+                        res.send({
+                          message: "Error updating user",
+                          errs: ["Error updating user"],
+                          result: false,
+                        });
+                      });
+                  }
+                })
+                .catch((errFOU) => {
+                  upLogger.error({ data: { err: errFOU } });
+                  res.send({
+                    message: "Error finding user",
+                    errs: ["Error finding user"],
+                    result: false,
+                  });
+                });
+            } else {
+              uplogger.error({
                 data: {
-                  isToday: isToday(new Date(user.lastUpdated)),
-                  lastUpdated: user.lastUpdated,
+                  message: "User does not exist",
+                  result: false,
                 },
               });
-              if (isToday(new Date(user.lastUpdated))) {
-                upLogger.info("User already updated today");
-                res.status(200).json({
-                  result: false,
-                  errs: ["User already updated today"],
-                  message: "User already updated today",
-                });
-              } else {
-                User.findOneAndUpdate(
-                  {},
-                  { lastUpdated: new Date().toUTCString() }
-                )
-                  .then((updatedUser) => {
-                    upLogger.info({
-                      data: {
-                        user: {
-                          lastUpdated: updatedUser.lastUpdated,
-                          marks: getMarks(updatedUser.wastedDays),
-                          wastedDays: updatedUser.wastedDays,
-                        },
-                        message: "Utilised successfully",
-                      },
-                    });
-                    res.send({
-                      user: updatedUser,
-                      message: "Utilised successfully",
-                      result: true,
-                    });
-                  })
-                  .catch((errFOU) => {
-                    upLogger.error({ data: { err: errFOU } });
-                    res.send({
-                      message: "Error updating user",
-                      errs: ["Error updating user"],
-                      result: false,
-                    });
-                  });
-              }
-            })
-            .catch((errFOU) => {
-              upLogger.error({ data: { err: errFOU } });
               res.send({
-                message: "Error finding user",
-                errs: ["Error finding user"],
+                message: "User does not exist",
                 result: false,
+                errs: ["User does not exist"],
               });
-            });
-        } else {
-          uplogger.error({
-            data: {
-              message: "User does not exist",
+            }
+          })
+          .catch((errFOU) => {
+            upLogger.error({ data: { err: errFOU } });
+            res.send({
+              message: "Error finding user",
+              errs: ["Error finding user"],
               result: false,
-            },
+            });
           });
-          res.send({
-            message: "User does not exist",
-            result: false,
-            errs: ["User does not exist"],
-          });
-        }
-      })
-      .catch((errFOU) => {
-        upLogger.error({ data: { err: errFOU } });
-        res.send({
-          message: "Error finding user",
-          errs: ["Error finding user"],
-          result: false,
-        });
+      }
+    })
+    .catch((err) => {
+      upLogger.error({ data: { err: err } });
+      res.send({
+        message: "Error connecting to database",
+        errs: ["Error connecting to database"],
+        result: false,
       });
-  }
+    });
 };
 export default upHandler;
